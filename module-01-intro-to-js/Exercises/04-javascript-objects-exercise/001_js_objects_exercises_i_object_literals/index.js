21c1f3128dd4c6ec41ea9f84abd5bdf38748a310
Renke Brixel <renke.brixel@gmail.com>
2026-05-13 16:41:27 +0200

    objects I corrections

--- module-01-intro-to-js/Exercises/04-javascript-objects-exercise/001_js_objects_exercises_i_object_literals/index.js
+++ module-01-intro-to-js/Exercises/04-javascript-objects-exercise/001_js_objects_exercises_i_object_literals/index.js
@@ -1,1 +1,27 @@
-// You can work here or download the template!
+const book = {
+  title: 'Dune',
+  author: 'Frank Herbert',
+  pages: 412,
+  isRead: true,
+
+  summary() {
+    const readStatus = this.isRead ? 'Yes' : 'No';
+    return `Title ${this.title}, Author ${this.author}, Pages: ${this.pages}, Read: ${readStatus}`;
+  },
+  toggleReadStatus() {
+    // this.isRead = this.isRead ? false : true;
+    this.isRead = !this.isRead;
+  },
+};
+
+console.log(book.summary());
+
+book.toggleReadStatus();
+
+console.log(book.summary());
+
+book.getAuthorInfo = function () {
+  return `${this.author} has written a book with ${this.pages} pages.`;
+};
+
+console.log(book.getAuthorInfo());
--- module-01-intro-to-js/Exercises/04-javascript-objects-exercise/002_js_objects_exercises_i_destructuring/index.js
+++ module-01-intro-to-js/Exercises/04-javascript-objects-exercise/002_js_objects_exercises_i_destructuring/index.js
@@ -1,8 +1,20 @@
-// You can work here or download the template!
 // Initial array
 const fruits = ['apple', 'banana', 'cherry', 'date'];
 
-// Initial object
+// # Simple Array Destructuring
+// const fruit1 = fruits[0];
+// const fruit2 = fruits[1];
+const [fruit1, fruit2] = fruits;
+console.log(fruit1, fruit2);
+
+// # Skipping Elements in Array Destructuring
+const [firstFruit, , thirdFruit] = fruits;
+// const { 0: firstFruit, 2: thirdFruit } = fruits;
+console.log(firstFruit, thirdFruit);
+
+// # -------------
+
+// # Initial object
 const person = {
   name: 'John Doe',
   age: 30,
@@ -10,8 +22,66 @@ const person = {
     city: 'New York',
     zip: '10001',
   },
+  // profession: 'Studend',
+  isActive: true,
+  toggleActiveStatus() {
+    this.isActive = !this.isActive;
+  },
 };
-// Initial function
-function displayPerson(person) {
-  console.log(`Name: ${person.name}, Age: ${person.age}`);
+
+console.log(person.isActive);
+person.toggleActiveStatus();
+console.log(person.isActive);
+
+// # Simple Object Destructuring
+// # Nested Object Destructuring
+// const {
+//   name,
+//   age,
+//   address: { city, zip: postalcode },
+//   profession = 'Unemployed',
+// } = person;
+
+// console.log(name, age, city, postalcode, profession);
+
+// const profession = person.profession || 'Unemployed';
+
+// const postalcode = person.address.zip;
+// console.log(postalcode);
+
+// const { address } = person;
+// const { city } = address;
+// console.log(city);
+
+// const {
+//   adress: { city },
+// } = person;
+
+// # -------------
+// # Initial function
+// # Destructuring in Function Parameters
+function displayPerson({
+  name: firstName = 'Unkown',
+  age = 'Unkown',
+  address: { zip: postalcode = 'Unkown' },
+  profession = 'Unemployed',
+}) {
+  console.log(`Name: ${firstName}, Age: ${age}, PLZ: ${postalcode}, Profession: ${profession}`);
 }
+
+displayPerson(person);
+// displayPerson(person2);
+// displayPerson(person3);
+
+// # ---
+
+function displayPerson2({ name, age, ...stegosaurus }) {
+  console.log(`Name: ${name}, Age: ${age}`);
+  console.log('Other data:', stegosaurus);
+}
+displayPerson2(person);
+
+function displayPerson3({ name, age }) {
+  console.log(`Name: ${name}, Age: ${age}`);
+}
+displayPerson3(person);

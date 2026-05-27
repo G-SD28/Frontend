d5927778c3bb7462688eb1e90795b46b5a163f22
Renke Brixel <renke.brixel@gmail.com>
2026-05-27 11:07:54 +0200

    add solution for modules app

---
+++ module-03-javascript-modules/Exercises/03_multi_module_app/index.js
@@ -0,0 +1,18 @@
+import { fetchProducts } from './modules/network.js';
+import { renderProdCard } from './modules/ui.js';
+
+const productsContainer = document.querySelector('#products-container');
+
+const renderProducts = async () => {
+  try {
+    const allProducts = await fetchProducts();
+
+    allProducts?.forEach((prod) => {
+      renderProdCard(prod, productsContainer);
+    });
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+renderProducts();
